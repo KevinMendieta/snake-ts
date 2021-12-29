b7ead1c9cb519295ee2f8f8f360ac9534f977296
KevinMendieta <kevin@wonderschool.com>
2021-12-29 16:50:09 -0500

    added husky pre-commit

--- src/ts/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const init = () => {
-    const container = document.getElementById('app');
-    const title = document.createElement('h1');
-    title.innerHTML = 'Snake Game';
-    container?.appendChild(title);
-    return null;
-};
-document.body.onload = init();
